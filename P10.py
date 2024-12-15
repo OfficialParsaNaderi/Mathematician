a35898b9d040861ee33216102152c93566c1b33c
OfficialParsaNaderi <parsanaderi1387.programmer@gmail.com>
2024-12-15 22:12:16 +0330

    I push all of My Calculation about Python .!

---
+++ P10.py
@@ -0,0 +1,6 @@
+from os import system
+system("cls")
+variable=list[str]
+number1=str(345)
+result1=number1
+print(result1)
---
+++ P9.py
@@ -0,0 +1,6 @@
+from os import system
+system("cls")
+variable=list[int]
+number1=int(+2.5)
+result1=number1
+print(result1)
